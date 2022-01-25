dbdd7e583dd16ba01164e86e100e6e4d46faed6d
jrodri <jrodri@elpuig.xeill.net>
2022-01-25 13:10:06 +0100

    v2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("tenemos 5 manzanas");
         System.out.println("nos roban 3 manzanas");
+        System.out.println(calcula(5, 3));
     }
-    public void calcula(){
 
+    public static int calcula(int a, int b){
     }
 }
