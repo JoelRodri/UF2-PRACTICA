a3a568e6f47454c8975facc76805a68be7a00303
jrodri <jrodri@elpuig.xeill.net>
2022-01-25 13:07:02 +0100

    v1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("tenemos 5 manzanas");
+        System.out.println("nos roban 3 manzanas");
     }
     public void calcula(){
 
