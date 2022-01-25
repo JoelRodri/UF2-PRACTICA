d07ab733418c7246329bf37c3a86c5f2fdd58116
jrodri <jrodri@elpuig.xeill.net>
2022-01-25 13:10:36 +0100

    v3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Tenemos 5 manzanas");
-    }
-    public void calcula(){
+        System.out.println("tenemos 5 manzanas");
+        System.out.println("nos roban 3 manzanas");
 
+    }
+    public static int calcula(int a, int b){
+        return a - b;
     }
 }
